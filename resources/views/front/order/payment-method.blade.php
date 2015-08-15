5fd53cf99a0ca514a212112cb2db6f5e8924fcff
Purvesh <ind.purvesh@gmail.com>
2015-08-14 22:22:03 -0700

    working on order steps

--- resources/views/front/order/payment-method.blade.php
+++ resources/views/front/order/payment-method.blade.php
@@ -2,70 +2,19 @@
 
 
 @section('content')
-    <h1>Checkout</h1>
+ {!! Form::open(array( 'url' => url('order'))) !!}
+ 
+ <?php //var_dump($config['shipping']); ?>
+<ul class="list-group">
+    @foreach($config['payment'] as $paymentIdentifier => $payment)
+    <li class="list-group-item">
+        <input type="radio" value="{{ $paymentIdentifier }}" name="payment_type"/> <label>{{ $payment['label'] }}</label>
+    </li>
+    @endforeach
 
-    {!! Form::model($data,array( 'url' => url('order'))) !!}
+</ul>
 
-    <div class="col-md-12">
-        <h3>Billing Information</h3>
-        
-        <div class="form-group">
-            {!!  Form::label('name', 'Name')  !!}
-            {!!  Form::text('user[name]',null,array('class'=>'form-control'))  !!}
-        </div>
 
-        <div class="form-group">
-            {!!  Form::label('companyname', 'Company Name')  !!}
-            {!!  Form::text('user[companyname]',null,array('class'=>'form-control'))  !!}
-        </div>
-
-        @if(!app('front.auth')->check())
-        <div class="form-group">
-            {!!  Form::label('email', 'Email')  !!}
-            {!!  Form::text('user[email]',null,array('class'=>'form-control'))  !!}
-        </div>
-        <div class="form-group">
-            {!!  Form::label('password', 'Password')  !!}
-            {!!  Form::text('user[password]',null,array('class'=>'form-control'))  !!}
-        </div>
-        @endif
-
-        <div class="form-group">
-            {!!  Form::label('number', 'Number')  !!}
-            {!!  Form::text('billing[number]',null,array('class'=>'form-control'))  !!}
-        </div>
-
-        <div class="form-group">
-            {!!  Form::label('street', 'Street')  !!}
-            {!!  Form::text('billing[street]',null,array('class'=>'form-control'))  !!}
-        </div>
-
-        <div class="form-group">
-            {!!  Form::label('area', 'Area')  !!}
-            {!!  Form::text('billing[area]',null,array('class'=>'form-control'))  !!}
-        </div>
-
-        <div class="form-group">
-            {!!  Form::label('city', 'City')  !!}
-            {!!  Form::text('billing[city]',null,array('class'=>'form-control'))  !!}
-        </div>
-
-        <div class="form-group">
-            {!!  Form::label('post_code', 'Post Code')  !!}
-            {!!  Form::text('billing[post_code]',null,array('class'=>'form-control'))  !!}
-        </div>
-
-        <div class="form-group">
-            {!!  Form::label('country', 'Country')  !!}
-            {!!  Form::text('billing[country]',null,array('class'=>'form-control'))  !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::hidden('step','shipping') !!}
-            {!! Form::button('continue',['class'=>'btn btn-primary','onclick' => 'jQuery(this).parents("form:first").submit()']) !!}
-        </div>
-    </div>
-
+ {!! Form::hidden('step','shipping') !!}
     {!! Form::close() !!}
-
 @endsection
--- resources/views/front/order/shipping-method.blade.php
+++ resources/views/front/order/shipping-method.blade.php
@@ -2,70 +2,18 @@
 
 
 @section('content')
-    <h1>Checkout</h1>
 
-    {!! Form::model($data,array( 'url' => url('order'))) !!}
-
-    <div class="col-md-12">
-        <h3>Billing Information</h3>
-        
-        <div class="form-group">
-            {!!  Form::label('name', 'Name')  !!}
-            {!!  Form::text('user[name]',null,array('class'=>'form-control'))  !!}
-        </div>
-
-        <div class="form-group">
-            {!!  Form::label('companyname', 'Company Name')  !!}
-            {!!  Form::text('user[companyname]',null,array('class'=>'form-control'))  !!}
-        </div>
-
-        @if(!app('front.auth')->check())
-        <div class="form-group">
-            {!!  Form::label('email', 'Email')  !!}
-            {!!  Form::text('user[email]',null,array('class'=>'form-control'))  !!}
-        </div>
-        <div class="form-group">
-            {!!  Form::label('password', 'Password')  !!}
-            {!!  Form::text('user[password]',null,array('class'=>'form-control'))  !!}
-        </div>
-        @endif
-
-        <div class="form-group">
-            {!!  Form::label('number', 'Number')  !!}
-            {!!  Form::text('billing[number]',null,array('class'=>'form-control'))  !!}
-        </div>
-
-        <div class="form-group">
-            {!!  Form::label('street', 'Street')  !!}
-            {!!  Form::text('billing[street]',null,array('class'=>'form-control'))  !!}
-        </div>
-
-        <div class="form-group">
-            {!!  Form::label('area', 'Area')  !!}
-            {!!  Form::text('billing[area]',null,array('class'=>'form-control'))  !!}
-        </div>
-
-        <div class="form-group">
-            {!!  Form::label('city', 'City')  !!}
-            {!!  Form::text('billing[city]',null,array('class'=>'form-control'))  !!}
-        </div>
-
-        <div class="form-group">
-            {!!  Form::label('post_code', 'Post Code')  !!}
-            {!!  Form::text('billing[post_code]',null,array('class'=>'form-control'))  !!}
-        </div>
-
-        <div class="form-group">
-            {!!  Form::label('country', 'Country')  !!}
-            {!!  Form::text('billing[country]',null,array('class'=>'form-control'))  !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::hidden('step','shipping') !!}
-            {!! Form::button('continue',['class'=>'btn btn-primary','onclick' => 'jQuery(this).parents("form:first").submit()']) !!}
-        </div>
-    </div>
-
+ {!! Form::open(array( 'url' => url('order'))) !!}
+<ul class="list-group">
+    @foreach($config['shipping'] as $shippingIdentifier => $shipping)
+    <li class="list-group-item">
+        <input type="radio" value="{{ $shippingIdentifier }}" name="shipping_type"/> <label>{{ $shipping['label'] }}</label>
+    </li>
+    @endforeach
+    
+     {!! Form::hidden('step','shipping') !!}
     {!! Form::close() !!}
+
+</ul>
 
 @endsection
